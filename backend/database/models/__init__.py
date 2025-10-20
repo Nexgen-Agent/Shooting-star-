5ef58c79eef8720aa6ccba8f1442e9b5b8349418
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-20 09:44:21 +0200

    Update __init__.py

--- backend/database/models/__init__.py
+++ backend/database/models/__init__.py
@@ -4,18 +4,25 @@ Database models package.
 
 from .user import User
 from .brand import Brand
-from .campaign import Campaign
+from .campaign import Campaign, CampaignInfluencer
 from .influencer import Influencer
 from .department import Department
 from .transaction import Transaction
 from .performance import Performance
+from .tip import Tip
+from .system_logs import SystemLog
+from .secrets import Secret
 
 __all__ = [
     "User",
     "Brand", 
     "Campaign",
+    "CampaignInfluencer",
     "Influencer",
     "Department",
-    "Transaction",
-    "Performance"
+    "Transaction", 
+    "Performance",
+    "Tip",
+    "SystemLog",
+    "Secret"
 ]
