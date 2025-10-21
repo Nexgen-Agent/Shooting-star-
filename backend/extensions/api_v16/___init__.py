f297b1e83a85f088b7d13b32d0a448dbc1dee41c
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-21 07:56:35 +0200

    Create ___init__.py

---
+++ backend/extensions/api_v16/___init__.py
@@ -0,0 +1,8 @@
+# extensions/api_v16/__init__.py
+"""
+API V16 Package - Versioned API endpoints for Shooting Star V16
+"""
+
+from .ai_router_v16 import router as ai_router_v16
+
+__all__ = ['ai_router_v16']
