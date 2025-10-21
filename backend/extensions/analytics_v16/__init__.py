63eca29913476d4263487670ae9cfaa0088d4058
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-21 07:55:20 +0200

    Create __init__.py

---
+++ backend/extensions/analytics_v16/__init__.py
@@ -0,0 +1,8 @@
+# extensions/analytics_v16/__init__.py
+"""
+Analytics V16 Package - Real-time insights and analytics for Shooting Star V16
+"""
+
+from .realtime_insights import realtime_insights, RealTimeInsightsV16
+
+__all__ = ['realtime_insights', 'RealTimeInsightsV16']
