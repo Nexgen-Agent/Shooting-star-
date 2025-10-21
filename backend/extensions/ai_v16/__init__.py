5ba4524fd25c8717b2966de171d753aa5729226b
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-21 07:54:50 +0200

    Create __init__.py

---
+++ backend/extensions/ai_v16/__init__.py
@@ -0,0 +1,9 @@
+# extensions/ai_v16/__init__.py
+"""
+AI V16 Package - Advanced AI capabilities for Shooting Star V16 Engine
+"""
+
+from .ai_supervisor import ai_supervisor, AISupervisorV16
+from .real_time_engine import real_time_engine, RealTimeEngineV16
+
+__all__ = ['ai_supervisor', 'AISupervisorV16', 'real_time_engine', 'RealTimeEngineV16']
