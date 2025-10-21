cff5f4c9f22273bab3d6cf25f5b8f6c0a1a42d01
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-21 07:57:08 +0200

    Create __init__.py

---
+++ backend/extensions/services_v16/__init__.py
@@ -0,0 +1,8 @@
+# extensions/services_v16/__init__.py
+"""
+Services V16 Package - Extended service layer for Shooting Star V16
+"""
+
+from .ai_dispatcher import ai_dispatcher, AIDispatcherV16
+
+__all__ = ['ai_dispatcher', 'AIDispatcherV16']
