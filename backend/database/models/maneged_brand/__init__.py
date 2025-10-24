f2b22cf5ed1b6a90b8571c9f18b4e0e58394692b
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-24 09:37:40 +0200

    Create __init__.py

---
+++ backend/database/models/maneged_brand/__init__.py
@@ -0,0 +1,6 @@
+from .brand_profile import BrandProfile
+from .campaign_history import CampaignHistory
+from .brand_finances import BrandFinances
+from .brand_tasks import BrandTask
+
+__all__ = ["BrandProfile", "CampaignHistory", "BrandFinances", "BrandTask"]
