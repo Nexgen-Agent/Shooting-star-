3992d46de087fc0395d9340755e9ce8d2d6ea1ab
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-24 10:15:00 +0200

    Create __init__.py

---
+++ backend/database/models/finance/__init__.py
@@ -0,0 +1,10 @@
+from .transaction import Transaction, ProfitAllocation, FinancialProjection
+from .performance import FinancialPerformance, ROITracking
+
+__all__ = [
+    "Transaction", 
+    "ProfitAllocation", 
+    "FinancialProjection",
+    "FinancialPerformance", 
+    "ROITracking"
+]
