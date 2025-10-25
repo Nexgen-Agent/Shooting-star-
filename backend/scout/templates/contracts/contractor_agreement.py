3abcb95492416ce245629bc367631ffbe40ed757
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-10-25 09:39:01 +0200

    Create templates/contracts/contractor_agreement.py

---
+++ backend/scout/templates/contracts/contractor_agreement.py
@@ -0,0 +1,25 @@
+# scout/templates/contracts/contractor_agreement.md
+# Independent Contractor Agreement
+
+This Independent Contractor Agreement ("Agreement") is made effective as of {effective_date} by and between:
+
+**ShootingStar** ("Company") and **{candidate_name}** ("Contractor").
+
+## 1. Services
+Contractor agrees to perform the following services: {role}
+
+## 2. Compensation
+Company agrees to pay Contractor as follows:
+- Type: {compensation_type}
+- Amount: {compensation_amount} {compensation_currency}
+- Equity: {equity_percentage}%
+- Duration: {duration_days}
+
+## 3. Term
+This Agreement shall commence on {start_date} and shall continue until terminated by either party.
+
+## 4. Independent Contractor
+Contractor is an independent contractor and not an employee of Company.
+
+## 5. Confidentiality
+Contractor agrees not to disclose any proprietary information of Company.
